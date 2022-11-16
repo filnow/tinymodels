cbcad56a6f89b13ab017ec5432206002eda1316e
filnow <filnow3@st.amu.edu.pl>
2022-11-16 17:02:55 +0100

    vgg19 torch

---
+++ vgg16/vgg19-torch.py
@@ -0,0 +1,107 @@
+#https://download.pytorch.org/models/vgg16_bn-6c64b313.pth
+#https://arxiv.org/pdf/1409.1556.pdf
+import torch
+import torch.nn as nn
+from PIL import Image
+from torchvision import transforms
+from torch.hub import load_state_dict_from_url
+import requests
+from io import BytesIO
+import sys
+
+
+class VGG19(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            
+            nn.Conv2d(3, 64, kernel_size=3, padding=1), # f0
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1), #f2
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1), #f5
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1), #f7
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1), #f10
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1), #f12
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1), #f14
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1), #f16
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1), #f19
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f21
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f23
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f25
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f28
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f30
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f32
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1), #f34
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+
+        )
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            
+            nn.Linear(25088, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 1000),
+    )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+model = VGG19()
+data = load_state_dict_from_url("https://download.pytorch.org/models/vgg19-dcbb9e9d.pth")
+model.load_state_dict(data)
+
+model.eval
+
+transform = transforms.Compose([            
+ 
+ transforms.Resize(256),                    
+ transforms.CenterCrop(224),                
+ transforms.ToTensor(),                     
+ transforms.Normalize(                      
+ mean=[0.485, 0.456, 0.406],                
+ std=[0.229, 0.224, 0.225]                  
+ 
+ )])
+
+#'https://raw.githubusercontent.com/srirammanikumar/DogBreedClassifier/master/images/Labrador_retriever_06457.jpg'
+response = requests.get(sys.argv[1])
+img = Image.open(BytesIO(response.content))
+img_t = transform(img)
+batch_t = torch.unsqueeze(img_t, 0)
+out = model(batch_t)
+labels = requests.get('https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt').text.split('\n')
+_, index = torch.max(out, 1)
+percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+
+print(labels[index[0]], percentage[index[0]].item())
