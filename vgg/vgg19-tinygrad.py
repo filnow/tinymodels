f10bb9fc7b99915900131025f171be04ba2103c5
filnow <filnow3@st.amu.edu.pl>
2022-11-17 14:32:51 +0100

    tiny vgg19

--- vgg/vgg19-tinygrad.py
+++ vgg/vgg19-tinygrad.py
@@ -5,84 +5,69 @@ from PIL import Image
 from torchvision import transforms
 import requests
 from io import BytesIO
-from extra.utils import get_parameters
+from extra.utils import get_parameters, get_child, fake_torch_load, fetch
 
-class AlexNet():
+class VGG19():
 
     def __init__(self):
         
-        self.features = (
+        self.features = {}
+        self.classifier = {}
 
-            (Tensor.glorot_uniform(64, 3, 3, 3), Tensor.zeros(64)),
-            (Tensor.glorot_uniform(64, 64, 3, 3), Tensor.zeros(64)),
-            (Tensor.glorot_uniform(128, 64, 3, 3), Tensor.zeros(128)),
-            (Tensor.glorot_uniform(128, 128, 3, 3), Tensor.zeros(128)),
-            (Tensor.glorot_uniform(256, 128, 3, 3), Tensor.zeros(256)),
-            (Tensor.glorot_uniform(256, 256, 3, 3), Tensor.zeros(256)),
-            (Tensor.glorot_uniform(256, 256, 3, 3), Tensor.zeros(256)),
-            (Tensor.glorot_uniform(256, 256, 3, 3), Tensor.zeros(256)),
-            (Tensor.glorot_uniform(512, 256, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512)),
-            (Tensor.glorot_uniform(512, 512, 3, 3), Tensor.zeros(512))
+        self.features_weight = {}
+        self.classifier_weight = {}
+        
 
-        )
-
-        self.classifier = (
-            
-            (Tensor.uniform(4096, 25088), Tensor.zeros(4096)),
-            (Tensor.uniform(4096, 4096), Tensor.zeros(4096)),
-            (Tensor.uniform(1000, 4096), Tensor.zeros(1000))
-    
-        )
+        self.features_bias = {}
+        self.classifier_bias = {}
+                
         
     def forward(self, x: Tensor) -> Tensor:
-         
-        x = x.conv2d(*self.conv0, padding=2, stride=4).relu().max_pool2d()
-        x = x.conv2d(*self.conv3, padding=2).relu().max_pool2d()
-        x = x.conv2d(*self.conv6, padding=1).relu()
-        x = x.conv2d(*self.conv8, padding=1).relu()
-        x = x.conv2d(*self.conv10, padding=1).relu().max_pool2d()
         
-        x = x.avg_pool2d(kernel_size=(1,1))
+        for i in range(16):
+            if i in [1,3,7,11,15]:
+                x = x.conv2d(*self.features[i], padding=1).relu().max_pool2d
+            else:
+                x = x.conv2d(*self.features[i], padding=1).relu()
+        
+        #x = x.avg_pool2d(kernel_size=(1,1))
  
         x = x.flatten(1)
         x = x.dropout(0.5)
         
-        x = x.linear(self.ll1[0].transpose(), self.ll1[1]).relu()
+        x = x.linear(self.classifier[0][0].transpose(), self.classifier[0][1]).relu()
         x = x.dropout(0.5)
         
-        x = x.linear(*self.ll4).relu()
-        x = x.linear(self.ll6[0].transpose(), self.ll6[1])
+        x = x.linear(*self.classifier[1]).relu()
+        x = x.linear(self.classifier[2][0].transpose(), self.classifier[2][1])
 
         return x
       
     def fake_load(self):
 
         data = load_state_dict_from_url('https://download.pytorch.org/models/vgg19-dcbb9e9d.pth', progress=True)
-        
-        for i in range(len(self.features)):
-            for j in data:
-                if 'weight' in j:                    
-                    self.features[i][0].assign(data[j].detach().numpy())
-                else:
-                    self.features[i][1].assign(data[j].detach().numpy())
-        
-        for i in range(len(self.classifier)):
-            for j in data:
-                if 'weight' in j:                    
-                    self.features[i][0].assign(data[j].detach().numpy())
-                else:
-                    self.features[i][1].assign(data[j].detach().numpy())
 
-m = AlexNet()
+        for k,v in data.items():
+            
+            if 'features' in k:    
+                if 'weight' in k:
+                    self.features_weight[k] = Tensor.uniform(*v.detach().numpy().shape).assign(v.detach().numpy())
+                else:
+                    self.features_bias[k] = Tensor.zeros(*v.detach().numpy().shape).assign(v.detach().numpy())
+            else:  
+                if 'weight' in k:
+                    self.classifier_weight[k] = Tensor.uniform(*v.detach().numpy().shape).assign(v.detach().numpy())
+                else:
+                    self.classifier_bias[k] = Tensor.zeros(*v.detach().numpy().shape).assign(v.detach().numpy())
+
+        self.features = tuple(zip(self.features_weight.values(), self.features_bias.values()))
+        self.classifier = tuple(zip(self.classifier_weight.values(), self.classifier_bias.values()))
+
+m = VGG19()
 m.fake_load()
 
-'''
+#'https://download.pytorch.org/models/vgg19-dcbb9e9d.pth'
+
 transform= transforms.Compose([            
  
  transforms.Resize(256),                    
@@ -107,4 +92,4 @@ _, index = torch.max(torch.tensor(out.numpy()), 1)
 percentage = torch.nn.functional.softmax(torch.tensor(out.numpy()), dim=1)[0] * 100
 
 print(labels[index[0]], percentage[index[0]].item())
-'''
+
