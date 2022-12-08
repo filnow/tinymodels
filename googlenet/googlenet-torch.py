dd02da0cde4e9871837d51221851fe3045992fa4
filnow <filnow3@st.amu.edu.pl>
2022-12-08 21:06:02 +0100

    almost right

--- googlenet/googlenet-torch.py
+++ googlenet/googlenet-torch.py
@@ -3,50 +3,48 @@ import torch.nn as nn
 from PIL import Image
 from torchvision import transforms
 from torch.hub import load_state_dict_from_url
+import torch.nn.functional as F
 import requests
 from io import BytesIO
 import sys
 
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs) -> None:
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x, inplace=True)
+
 class Inception(nn.Module):
     
-    def __init__(self) -> None:
+    def __init__(self, in_channels, ch1x1, ch3x3red, ch3x3, ch5x5red, ch5x5, pool_proj) -> None:
         super().__init__()
-        self.branch1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=1, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
-        )
+        self.branch1 = BasicConv2d(in_channels, ch1x1, kernel_size=1)
         self.branch2 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=1, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(3, 64, kernel_size=3, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
+            BasicConv2d(in_channels, ch3x3red, kernel_size=1),
+            BasicConv2d(ch3x3red, ch3x3, kernel_size=3, padding=1)
         )
         self.branch3 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=1, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(3, 64, kernel_size=5, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
+            BasicConv2d(in_channels, ch5x5red, kernel_size=1),
+            BasicConv2d(ch5x5red, ch5x5, kernel_size=3, padding=1)
         )
         self.branch4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1),
-            nn.Conv2d(3, 64, kernel_size=1, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
+            BasicConv2d(in_channels, pool_proj, kernel_size=1)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
-        b1 = self.branch1(x.clone())
-        b2 = self.branch2(x.clone())
-        b3 = self.branch3(x.clone())
-        b4 = self.branch4(x.clone())
+        b1 = self.branch1(x)
+        b2 = self.branch2(x)
+        b3 = self.branch3(x)
+        b4 = self.branch4(x)
 
-        x = torch.cat((b1,b2,b3,b4), 0)
+        x = torch.cat([b1,b2,b3,b4], 1)
 
         return x
 
@@ -55,36 +53,28 @@ class GoogleNET(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.conv1  = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            BasicConv2d(3, 64, kernel_size=7, stride=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.LocalResponseNorm(64)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=1, stride=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
+            BasicConv2d(64, 64, kernel_size=1, stride=1)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 192, kernel_size=3, stride=1),
-            nn.BatchNorm2d(192),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(192),
+            BasicConv2d(64, 192, kernel_size=3, stride=1),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         
-        self.inception3a = Inception()
-        self.inception3b = Inception()
+        self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = Inception(256, 128, 128, 192, 32, 96, 64)
+
+        self.inception4a = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = Inception(528, 256, 160, 320, 32, 128, 128)
         
-        self.inception4a = Inception()
-        self.inception4b = Inception()
-        self.inception4c = Inception()
-        self.inception4d = Inception()
-        self.inception4e = Inception()
-        
-        self.inception5a = Inception()
-        self.inception5b = Inception()
+        self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
