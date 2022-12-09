85f8fbdc9d5f9e55a8fb2e865910ae3aafe7ca61
filnow <filnow3@st.amu.edu.pl>
2022-12-09 19:38:03 +0100

    googlenet works

--- googlenet/googlenet-torch.py
+++ googlenet/googlenet-torch.py
@@ -8,6 +8,27 @@ import requests
 from io import BytesIO
 import sys
 
+
+class AuxInception(nn.Module):
+    def __init__(self, in_channels) -> None:
+        super().__init__()
+        self.conv = BasicConv2d(in_channels, 128, kernel_size=1)
+
+        self.fc1 = nn.Linear(2048, 1024)
+        self.fc2 = nn.Linear(1024, 1000)
+        self.dropout = nn.Dropout(p=0.7)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = F.adaptive_avg_pool2d(x, (4, 4))
+        x = self.conv(x)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x), inplace=True)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        
+        return x
+
+
 class BasicConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, **kwargs) -> None:
         super().__init__()
@@ -33,7 +54,7 @@ class Inception(nn.Module):
             BasicConv2d(ch5x5red, ch5x5, kernel_size=3, padding=1)
         )
         self.branch4 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
             BasicConv2d(in_channels, pool_proj, kernel_size=1)
         )
 
@@ -50,13 +71,13 @@ class Inception(nn.Module):
 
 class GoogleNET(nn.Module):
     
-    def __init__(self) -> None:
+    def __init__(self, aux_logits: bool = True) -> None:
         super().__init__()
-        self.conv1  = BasicConv2d(3, 64, kernel_size=7, stride=2)
+        self.conv1  = BasicConv2d(3, 64, kernel_size=7, stride=2, padding=3)
 
-        self.conv2 = BasicConv2d(64, 64, kernel_size=1, stride=1)
+        self.conv2 = BasicConv2d(64, 64, kernel_size=1)
         
-        self.conv3 = BasicConv2d(64, 192, kernel_size=3, stride=1)
+        self.conv3 = BasicConv2d(64, 192, kernel_size=3, padding=1)
             
         self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
         self.inception3b = Inception(256, 128, 128, 192, 32, 96, 64)
@@ -69,6 +90,10 @@ class GoogleNET(nn.Module):
         
         self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
         self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
+
+        if aux_logits:
+            self.aux1 = AuxInception(512)
+            self.aux2 = AuxInception(528)
 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -91,9 +116,15 @@ class GoogleNET(nn.Module):
         x = self.maxpool(x)
 
         x = self.inception4a(x)
+
+        aux1 = self.aux1(x)
+
         x = self.inception4b(x)
         x = self.inception4c(x)
         x = self.inception4d(x)
+
+        aux2 = self.aux2(x)
+
         x = self.inception4e(x)
         x = self.maxpool(x)
 
@@ -114,4 +145,31 @@ model = GoogleNET()
 data = load_state_dict_from_url('https://download.pytorch.org/models/googlenet-1378be20.pth')
 model.load_state_dict(data)
 
+model.eval()
+
+transform = transforms.Compose([            
+ 
+ transforms.Resize(256),                    
+ transforms.CenterCrop(224),                
+ transforms.ToTensor(),                     
+ transforms.Normalize(                      
+ mean=[0.485, 0.456, 0.406],                
+ std=[0.229, 0.224, 0.225]                  
+ 
+ )])
+
+#'https://raw.githubusercontent.com/srirammanikumar/DogBreedClassifier/master/images/Labrador_retriever_06457.jpg'
+response = requests.get('https://raw.githubusercontent.com/srirammanikumar/DogBreedClassifier/master/images/Labrador_retriever_06457.jpg')
+img = Image.open(BytesIO(response.content))
+img_t = transform(img)
+batch_t = torch.unsqueeze(img_t, 0)
+out = model(batch_t)
+labels = requests.get('https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt').text.split('\n')
+_, index = torch.max(out, 1)
+percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+#import matplotlib.pyplot as plt
+#plt.plot(percentage.detach().numpy())
+#plt.show()
+print(index[0])
+print(labels[index[0]], percentage[index[0]].item())
 #https://github.com/pytorch/vision/blob/main/torchvision/models/googlenet.py
