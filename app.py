a61a87749265577685bd9e323758cffdd5cdae1b
filnow <filnow3@st.amu.edu.pl>
2022-12-22 11:50:37 +0100

    redirect

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models import *
 from utils import class_img, run_model
 
 app = Flask(__name__, template_folder='./frontend/templates',static_folder='./frontend/static')
-@app.route('/', methods=['GET'])
-def hello():
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    
+    if request.method == 'POST':
+
+        model_name = request.form.get('myInput', False)
+        
+        imagefile = request.files['imagefile']
+        image_path = "./images/" + imagefile.filename
+        imagefile.save(image_path)
+        
+        class_name, precentage = class_img(run_model(model_name), image_path)
+        
+        prediction = f'Image is {class_name} in ({precentage:.2f}%) predicted by model: {model_name}'
+    
+        return redirect(url_for('success', prediction=prediction))
+    
     return render_template('index.html')
-@app.route('/', methods=['POST'])
-def predict():
-    
-    model_name = request.form.get('myInput', False)
-    
-    imagefile = request.files['imagefile']
-    image_path = "./images/" + imagefile.filename
-    imagefile.save(image_path)
-    
-    class_name, precentage = class_img(run_model(model_name), image_path)
-    
-    classification = f'Image is {class_name} in ({precentage:.2f}%) predicted by model: {model_name}'
-    
-    return render_template('index.html', prediction=classification)
+
+@app.route('/success')
+def success():
+
+    prediction = request.args.get('prediction')
+    return render_template('success.html', prediction=prediction)
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
